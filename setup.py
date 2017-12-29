d0e02b6bb11f5574a42548ac879050d46e37b1fe
Gus Smith <guscomps@gmail.com>
2017-12-29 02:17:25 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""A Python library for accessing a capstats server.
+"""
+
+from setuptools import setup
+
+setup(
+    name = 'capstats-client',
+    version = '0.1.0',
+    description = 'A Python library for accessing a capstats server',
+    author = 'Gus Smith',
+    packages = ['capstats-client']
+)
